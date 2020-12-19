61ada04adc5865c013bb03078b401f8ef17c41ce
Sultan Ahmad <saksaikot@gmail.com>
2020-12-19 23:00:07 +0600

    async js exercise beautified

--- 6. Asynchronous JavaScript/exercise/index.js
+++ 6. Asynchronous JavaScript/exercise/index.js
@@ -1,66 +1,67 @@
-const users=[
+const users = [
     {
-        name:"saikot",
-        email:"saksaikot@gmail.com",
-        isGold:true
+        name: "saikot",
+        email: "saksaikot@gmail.com",
+        isGold: true
     }
 ];
-const topMovies=[
+const topMovies = [
     {
-        name:"movie1"
+        name: "movie1"
     },
     {
-        name:"movie2"
+        name: "movie2"
     }
     ,
     {
-        name:"movie3"
+        name: "movie3"
     }
     ,
     {
-        name:"movie4"
+        name: "movie4"
     }
 
 ];
-function getUser(id){
-    return new Promise((res,rej)=>{
-        setTimeout(()=>{
+function getUser(id) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
             console.log(`Getting user ${id} from db ...`);
             res(users[id]);
-        },2000)
+        }, 2000)
     });
 }
 
-function getTopMovies(){
-    return new Promise((res,rej)=>{
+function getTopMovies() {
+    return new Promise((res, rej) => {
 
-        setTimeout(()=>{
+        setTimeout(() => {
             console.log(`Getting top from db ...`);
             res(topMovies);
             // rej(new Error("getting movie list failed"));
-        },2000)
+        }, 2000)
     });
 }
 
-function sendEmail(email,movieList){
-    return new Promise((res,rej)=>{
-        setTimeout(()=>{
-            console.log(`sending the list ${JSON.stringify(movieList) } to email ${email}`);
+function sendEmail(email, movieList) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log(`sending the list ${JSON.stringify(movieList)} to email ${email}`);
             res(true);
-        },2000)
+        }, 2000)
     });
 
 }
 
-async function topMoviesForGoldUser(){
-    try{    const user= await getUser(0);
-        if(user.isGold){
-            const topMovieList= await getTopMovies();
-            await sendEmail(user.email,topMovieList);
+async function topMoviesForGoldUser() {
+    try {
+        const user = await getUser(0);
+        if (user.isGold) {
+            const topMovieList = await getTopMovies();
+            await sendEmail(user.email, topMovieList);
         }
     }
-    catch(err){
-        console.log(`there was an error`,err);
+    catch (err) {
+        console.log(`there was an error`, err);
     }
 
 }
